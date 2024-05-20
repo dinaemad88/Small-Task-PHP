3b2c968f37af025105fba35d983c12cfe1f2cdb7
dina emad <dinaemad287@gmail.com>
2024-05-21 00:32:07 +0300

    update

--- navbar.php
+++ navbar.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <nav class="navbar navbar-expand-lg bg-body-tertiary pt-3">
   <div class="container-fluid">
 
@@ -20,36 +19,3 @@
 
   </div>
 </nav>
-=======
-<nav class=
-"navbar navbar-expand-lg 
-bg-body-tertiary pt-3">
-  <div class=
-  "container-fluid">
-
-    <h1><a class=
-    "navbar-brand"
-     href="index.php">
-     👋 Welcome</a></h1>
-    <ul
-     class="navbar-nav
-      me-auto mb-2 mb-lg-0">
-   
-</ul>
-    <form class="d-flex" >
-        <a class="btn
-         btn-outline-success mx-2" 
-         type="submit" 
-         href="register.php">Register</a>
-        <a class="btn btn-outline-primary mx-2"
-         type="submit" 
-         href="login.php">Login</a>
-        <a class="btn btn-outline-danger mx-2"
-         type="submit" 
-         href="logout.php">Logout</a>
-    </form>
-    
-
-  </div>
-</nav>
->>>>>>> origin/main
