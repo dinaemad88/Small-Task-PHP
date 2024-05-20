75db8bedd5bdab465f89fc44266c0f650075e067
dina emad <dinaemad287@gmail.com>
2024-05-21 00:18:45 +0300

    upload

--- navbar.php
+++ navbar.php
@@ -1,31 +1,21 @@
-<nav class=
-"navbar navbar-expand-lg 
-bg-body-tertiary pt-3">
-  <div class=
-  "container-fluid">
+<nav class="navbar navbar-expand-lg bg-body-tertiary pt-3">
+  <div class="container-fluid">
 
-    <h1><a class=
-    "navbar-brand"
-     href="index.php">
-     👋 Welcome</a></h1>
-    <ul
-     class="navbar-nav
-      me-auto mb-2 mb-lg-0">
+    <h1><a class="navbar-brand" href="index.php">👋 Welcome</a></h1>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
    
-</ul>
-    <form class="d-flex" >
-        <a class="btn
-         btn-outline-success mx-2" 
-         type="submit" 
-         href="register.php">Register</a>
-        <a class="btn btn-outline-primary mx-2"
-         type="submit" 
-         href="login.php">Login</a>
-        <a class="btn btn-outline-danger mx-2"
-         type="submit" 
-         href="logout.php">Logout</a>
+    </ul>
+    <form class="d-flex">
+        <?php
+            session_start();
+            if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+                echo '<a class="btn btn-outline-danger mx-2" type="submit" href="logout.php">Logout</a>';
+            } else {
+                echo '<a class="btn btn-outline-success mx-2" type="submit" href="register.php">Register</a>';
+                echo '<a class="btn btn-outline-primary mx-2" type="submit" href="login.php">Login</a>';
+            }
+        ?>
     </form>
-    
 
   </div>
 </nav>
